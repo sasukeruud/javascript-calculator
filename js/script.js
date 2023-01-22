3594e49a056ca4cd892f1e97918293bd2dc2c7c0
Robin Ruud Kristensen <robin1998@hotmail.no>
2023-01-22 16:12:36 +0100

    added mathematical functions

---
+++ js/script.js
@@ -0,0 +1,31 @@
+/**
+ * Addition function
+ * @param {*x first number} x 
+ * @param {*y second number} y 
+ * @returns the calcutlated value after addition have been done on the two numbers
+ */
+const add = (x,y) => x + y;
+
+/**
+ * Function for subraction
+ * @param {*x first number} x 
+ * @param {*y second number} y 
+ * @returns returns the subtracted value of x minus y
+ */
+const subtract = (x,y) => x - y;
+
+/**
+ * Function for mulitiplicaiton
+ * @param {*x first number} x 
+ * @param {*y second number} y 
+ * @returns returns the value of the number after multiplication have been done
+ */
+const multiply = (x,y) => x * y;
+
+/**
+ * Function to divide two numbers
+ * @param {*x first number} x 
+ * @param {*y second number} y 
+ * @returns returns the value after dividation have been done
+ */
+const divide = (x,y) => x / y;
